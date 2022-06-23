690636dcc9ecf99ea84a3edffaaf8b84690e0efd
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-23 19:34:04 +0600

    genPassword && validPassword function implemented

--- config/passport.js
+++ config/passport.js
@@ -44,20 +44,3 @@ passport.deserializeUser((userId, done) => {
     })
     .catch((err) => done(err));
 });
-
-// passport.use(
-//   new LocalStrategy(function (username, password, done) {
-//     User.findOne({ username: username }, function (err, user) {
-//       if (err) {
-//         return done(err);
-//       }
-//       if (!user) {
-//         return done(null, false);
-//       }
-//       if (!user.verifyPassword(password)) {
-//         return done(null, false);
-//       }
-//       return done(null, user);
-//     });
-//   })
-// );
